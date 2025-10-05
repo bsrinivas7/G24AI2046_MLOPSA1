c4991d80d95c59c7500bc046ab3513d6196b9813
bsrinivas7 <supersrinivas463@gmail.com>
2025-10-05 15:57:49 +0530

    Update train2.py

--- train2.py
+++ train2.py
@@ -1,8 +1,6 @@
+from misc import run_training_pipeline
 from sklearn.kernel_ridge import KernelRidge
-from misc import load_data, preprocess_data, train_and_evaluate
 
-df = load_data()
-X_train, X_test, y_train, y_test = preprocess_data(df)
-model = KernelRidge()
-mse = train_and_evaluate(model, X_train, X_test, y_train, y_test)
-print(f"KernelRidge Test MSE: {mse:.2f}")
+if __name__ == "__main__":
+    print("__Running KernelRidge Training Pipeline__")
+    run_training_pipeline(KernelRidge)
