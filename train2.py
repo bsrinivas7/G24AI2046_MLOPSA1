ee2697d39976d35d398d61f05b5a45e1e4f7bd4d
bsrinivas7 <supersrinivas463@gmail.com>
2025-10-05 15:45:47 +0530

    Delete train2.py

--- train2.py
+++
@@ -1,8 +0,0 @@
-from sklearn.kernel_ridge import KernelRidge
-from misc import load_data, preprocess_data, train_and_evaluate
-
-df = load_data()
-X_train, X_test, y_train, y_test = preprocess_data(df)
-model = KernelRidge()
-mse = train_and_evaluate(model, X_train, X_test, y_train, y_test)
-print(f"KernelRidge Test MSE: {mse:.2f}")
