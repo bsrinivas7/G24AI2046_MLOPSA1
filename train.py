d70b83bd36e93800bf21e1afd70ef364136ba44f
Srinivasb <supersrinivas463@gmail.com>
2025-10-11 10:08:00 +0530

    remove

--- train.py
+++
@@ -1,6 +0,0 @@
-from misc import run_training_pipeline
-from sklearn.tree import DecisionTreeRegressor
-
-if __name__ == "__main__":
-    print("__Running DecisionTreeRegressor Training Pipeline__")
-    run_training_pipeline(DecisionTreeRegressor)
